1ced0e24425ef991b73fe8b9a6a2e0d350eea0f3
isaacgarnand <isaac.garnand@gmail.com>
2020-01-17 14:07:04 -0800

    editted out some unnessesary line breaks, values for radio buttons, and editted python description

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     event.preventDefault();
-    
     var mathInteger = parseInt($("input#mathint").val());
     var myProblem = document.getElementById("trouble");
     var animals = parseInt($("#animal").val());
-
     var frustration = (mathInteger + animals)
-
     if (frustration >= 8) {
       $("#output0").show();
     }
@@ -17,14 +14,5 @@ $(document).ready(function() {
     else {
       $("#output2").show();
     }
-
-
-
-
-    
-
-
-
-
   });
 });
