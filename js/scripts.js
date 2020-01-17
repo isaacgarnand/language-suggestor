a6ed408c2ac4edc637c9479059f94490de0c590c
isaacgarnand <isaac.garnand@gmail.com>
2020-01-17 10:04:49 -0800

    added simple branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
-  $("form#quiz").submit(function() {
+  $("form#quiz").submit(function(event) {
     event.preventDefault();
+    
+    var mathIntger = parseInt($("input#mathint").val());
+    var myProblem = $("input:radio:[name=problem]:checked").val();
+    var animals = parseInt($("#animal").val());
+
+    var frustration = (mathIntger + animals);
+
+    if (frustration >= 8) {
+      $("#output0").show();
+    }
+    else if (frustration < 8 && myProblem === 'trouble') {
+      $("#output1").show();
+    }
+    else {
+      $("#output2").show();
+    }
+
+
+
 
     
 
